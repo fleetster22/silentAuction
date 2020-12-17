68c270e955339cfbc490d566c23b9a2c144901a0
fleetster22 <fleetster22@gmail.com>
2020-12-17 14:02:41 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Day 9 of 100 Days of Code - The Complete Python Pro Bootcamp for 2021
+from time import sleep
+from art import logo
+
+print(logo)
+sleep(0.5)
+print("Welcome to the silent auction program.")
+sleep(1)
+name = input("What is your name? ")
