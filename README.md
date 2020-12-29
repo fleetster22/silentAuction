cc75ee787b136af3672f45bcebff880b20c42eca
fleetster22 <fleetster22@gmail.com>
2020-12-29 12:54:44 -0500

    replace values in a dictionary

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Day 9 of 100 Days of Code - The Complete :snake: Pro Bootcamp for 2021
 ## Angela Yu Udemy Course
 ## silentAuction :octocat:
+Dictionaries and nesting
--- main.py
+++ main.py
@@ -2,8 +2,52 @@
 from time import sleep
 from art import logo
 
-print(logo)
-sleep(0.5)
-print("Welcome to the silent auction program.")
-sleep(1)
-name = input("What is your name? ")
+# programming_dictionary = {
+#     "Bug": "A spider in your computer",
+#     "Function": "A piece of code that you can easily call repeatedly",
+#     "Loop": "The action of doing something repeatedly",
+# }
+# programming_dictionary["Nested"] = "A unit of code that is fully contained under another unit of code"
+
+# print(programming_dictionary["Function"])
+# edit an entry
+# programming_dictionary["Bug"] = "An error in a program that prevents the program from running as expected"
+# print(programming_dictionary)
+
+# for key in programming_dictionary:
+#     print(key)
+#     print(programming_dictionary[key])
+
+student_scores = {
+    "Harry" : 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+
+student_grades = {}
+
+for student in student_scores:
+    if 90 < student_scores[student] <= 100:
+        student_grades[student] = "Outstanding"
+    elif 80 < student_scores[student] <= 90:
+        student_grades[student] = "Exceeds Expectations"
+    elif 70 < student_scores[student] <= 80:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
+
+print(student_grades)
+
+
+# print(logo)
+# sleep(0.75)
+# print("Welcome to the Silent Auction Program.")
+# sleep(0.5)
+# name = input("What is your name?\n")
+# sleep(0.15)
+# print(f"Welcome, {name}")
+# sleep(0.5)
+# bid = input("What is your bid on this item? \n$")
+# other_bidders = input("Are there any other bidders? Type 'yes' or 'no'\n")
